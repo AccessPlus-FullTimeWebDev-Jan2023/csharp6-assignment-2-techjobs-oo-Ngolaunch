6086e30cb4764ee96ec8104f1e4332456838569a
ngohile <ngohile.yakubu@gmail.com>
2023-03-27 00:13:10 -0500

    !

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -6,12 +6,26 @@ namespace TechJobs.Tests
 	{
         //Testing Objects
         //initalize your testing objects here
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            Job job1 = new Job();
+            Job job2 = new Job();
+            int iD
+            Assert.AreEqual(job1, job2);
         }
+
     }
+
+    
+    
 }
 
